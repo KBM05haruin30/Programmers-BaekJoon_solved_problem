d677e866b7236fa10c87c4dd8d98a72a4f963de2
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-11 12:00:47 +0900

    [Silver V] Title: 영화감독 숌, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, ans, cnt, temp;
+
+int main(){
+    cin >> N;
+    ans = 0;
+    cnt = 0;
+    while(cnt != N){
+        ans++;
+        temp = ans;
+        while(temp != 0){
+            if(temp % 1000 == 666){
+                cnt++;
+                break;
+            }
+            else{
+                temp /= 10;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
