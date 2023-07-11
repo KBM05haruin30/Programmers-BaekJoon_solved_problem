2ba558c172668db0c7f57f2bd54e2b61398d3052
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-12 00:45:05 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> str_list) {
+    vector<string> answer;
+    int rindex = -1;
+    int lindex = -1;
+    
+    for(int i = 0; i < str_list.size(); i++){
+        if(str_list[i] == "l"){
+            lindex = i;
+            break;
+        }
+        if(str_list[i] == "r"){
+            rindex = i;
+            break;
+        }
+    }
+    
+    if(lindex != -1 && rindex == -1){
+        answer.insert(answer.begin(),str_list.begin(),str_list.begin() + lindex);
+    }
+    else if(lindex == -1 && rindex != -1){
+        answer.insert(answer.begin(), str_list.begin() + rindex + 1, str_list.end());
+    }
+    
+    return answer;
+}
