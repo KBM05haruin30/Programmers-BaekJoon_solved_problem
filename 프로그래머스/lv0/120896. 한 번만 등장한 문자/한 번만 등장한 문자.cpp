99bfc42cbaf29b9d832eb78d140221982e87b009
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-14 10:35:09 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int arr[26] ={0,};
+    for(int i = 0; i < s.length(); i++){
+        arr[s[i]-'a']++;
+    }
+    for(int i = 0; i < 26; i++){
+        if(arr[i] == 1){
+            answer += (i + 'a');
+        }
+    }
+    return answer;
+}
