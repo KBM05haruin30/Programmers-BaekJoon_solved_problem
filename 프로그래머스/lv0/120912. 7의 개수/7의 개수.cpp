1d1392a9d8efb77846d59c97978974ebbc69f73b
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-18 00:40:25 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    for(int i = 0; i < array.size(); i++){
+        string s = to_string(array[i]);
+        for(int j = 0; j < s.length(); j++){
+            if(s[j] == '7'){
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
