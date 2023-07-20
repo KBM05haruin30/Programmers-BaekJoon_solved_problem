6670f0c365e19880e642fa193b5ab59e04d7f89e
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-21 00:33:25 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> delete_list) {
     vector<int> answer;
-    sort(delete_list.begin(),delete_list.end());
-    copy_if(arr.begin(),arr.end(),back_inserter(answer),[&](auto& num){
-        return !binary_search(delete_list.begin(),delete_list.end(),num);
-    });
+    for(int i = 0; i < delete_list.size(); i++){
+        for(int j = 0; j < arr.size(); j++){
+            if(arr[j] == delete_list[i]){
+                arr.erase(arr.begin() + j);
+            }
+        }
+    }
+    answer = arr;
     return answer;
 }
