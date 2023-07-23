bf85c0b5b25e6b86285a887abc30f3c0833882cb
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-23 14:38:30 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    for(int i = 0; i < str_list.size(); i++){
+        if(str_list[i].find(ex) != string::npos){
+            continue;
+        }
+        else{
+            answer += str_list[i];
+        }
+    }
+    return answer;
+}
