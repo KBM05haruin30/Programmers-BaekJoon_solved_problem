5a1f75f9bac8b4e2a40384e4e3d755b4eb1d5801
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-07-28 00:04:11 +0900

    [level 2] Title: 전화번호 목록, Time: 426.57 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    unordered_map<string,int> m;
+    for(int i = 0; i < phone_book.size();i++){
+        m[phone_book[i]]=1;
+    }
+    for(int i = 0; i < phone_book.size();i++){
+        for(int j = 0; j < phone_book[i].size()-1;j++){
+            string phone_number = phone_book[i].substr(0,j+1);
+            if(m[phone_number]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
