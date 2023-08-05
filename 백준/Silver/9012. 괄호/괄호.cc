e207464cab49ead7fba757fc429c93233c75f6ca
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2023-08-06 00:34:40 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool Check(string str){
+    int len = (int)str.length();
+    stack<char> st;
+    
+    for(int i = 0; i < len; i++){
+        char c = str[i];
+        
+        if(c == '('){
+            st.push(str[i]);
+        }
+        else{
+            if(!st.empty()){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string str;
+        cin >> str;
+        if(Check(str)){
+            cout << "YES" << '\n';
+        }else{
+            cout << "NO" << '\n';
+        }
+    }
+}
