90f7dae7550bc8b8dac4c20340580b8bb82adf57
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-03-21 15:51:55 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -1,39 +1,18 @@
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 string solution(int a, int b) {
     string answer = "";
-    map<int, string> yoil;
-    yoil.insert({2,"SUN"});
-    yoil.insert({3,"MON"});
-    yoil.insert({4,"TUE"});
-    yoil.insert({5,"WED"});
-    yoil.insert({6,"THU"});
-    yoil.insert({0,"FRI"});
-    yoil.insert({1,"SAT"});
-    map<int, int> days;
-    days.insert({1,31});
-    days.insert({2,29});
-    days.insert({3,31});
-    days.insert({4,30});
-    days.insert({5,31});
-    days.insert({6,30});
-    days.insert({7,31});
-    days.insert({8,31});
-    days.insert({9,30});
-    days.insert({10,31});
-    days.insert({11,30});
-    days.insert({12,31});
+    string yoil[] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    int days[] = {31,29,31,30,31,30,31,31,30,31,30,31};
     int day = 0;
-    for(int i = 1; i < a; i++){
+    for(int i = 0; i < (a-1); i++){
         day += days[i];
     }
-    day += b;
-    int what = (day-1) % 7;
-    answer = yoil[what];
-        
+    day += (b-1);
+    day %= 7;
+    answer = yoil[day];
     return answer;
 }
