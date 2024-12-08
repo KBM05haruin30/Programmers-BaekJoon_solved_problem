3ffe2cd1e40e04848e0335d34e2420ee215fb752
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-08 15:32:48 +0900

    [Silver III] Title: N과 M (2), Time: 168 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            for (int cand = selected[k-1] + 1; cand <= N; cand++) {
+                selected[k] = cand;
+                // k+1 번 ~ M 번을 모두 탐색하는 일을 해야 하는 상황
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+}
