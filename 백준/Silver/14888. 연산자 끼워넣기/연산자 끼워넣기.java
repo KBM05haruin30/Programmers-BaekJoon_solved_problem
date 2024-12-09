6e4a9dce808df49e9e900af37e85bb57f8f30646
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-09 18:42:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 172 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 public class Main {
@@ -21,53 +20,49 @@ public class Main {
         min = Integer.MAX_VALUE;
     }
 
-    // 완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
-    static int calculator() {
-        // nums, order
-        int value = nums[1];
-        for (int i = 1; i <= N - 1; i++) {
-            // value, order[i], num[i+1]
-            if (order[i] == 1) // +
-                value = value + nums[i+1];
-            if (order[i] == 2) // -
-                value = value - nums[i+1];
-            if (order[i] == 3) // *
-                value = value * nums[i+1];
-            if (order[i] == 4) // /
-                value = value / nums[i+1];
-        }
-        return value;
+    // 피연산자 2개와 연산자가 주어졌을 때 계산해주는 함수
+    static int calculator(int operand1, int operator, int operand2) {
+        // value, order[i], num[i+1]
+        if (operator == 1) // +
+            return operand1 + operand2;
+        else if (operator == 2) // -
+            return operand1 - operand2;
+        else if (operator == 3) // *
+            return operand1 * operand2;
+        else // /
+            return operand1 / operand2;
     }
 
 
-// order[1...N-1] 에 연산자들이 순서대로 저장될 것이다.
-static void rec_func(int k) {
-    if (k == N) { // 모든 연산자들을 전부 나열하는 방법을 찾은 상태
-        //	완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
-        int value = calculator();
-        max = Math.max(max, value);
-        min = Math.min(min, value);
-    } else {
-        // k 번째 연산자는 무엇을 선택할 것인가?
-        for (int cand = 1; cand <= 4; cand++) {
-            if (operators[cand] >= 1) {
-                operators[cand]--;
-                order[k] = cand;
-                rec_func(k+1);
-                operators[cand]++;
-                order[k] = 0;
+    // order[1...N-1] 에 연산자들이 순서대로 저장될 것이다.
+    static void rec_func(int k, int value) {
+        if (k == N) { // 모든 연산자들을 전부 나열하는 방법을 찾은 상태
+            //	완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
+            // int value = calculator();
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+        } else {
+            // k 번째 연산자는 무엇을 선택할 것인가?
+            for (int cand = 1; cand <= 4; cand++) {
+                if (operators[cand] >= 1) {
+                    operators[cand]--;
+                    order[k] = cand;
+                    int new_value = calculator(value, cand, nums[k + 1]);
+                    rec_func(k+1, new_value);
+                    operators[cand]++;
+                    order[k] = 0;
+                }
             }
+            // 4 가지의 연산자들 중에 뭘 쓸 것인지 선택하고 재귀호출하기
         }
-        // 4 가지의 연산자들 중에 뭘 쓸 것인지 선택하고 재귀호출하기
     }
-}
 
 
     // 실행하기
     public static void main(String[] args) {
         input();
         // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
-        rec_func(1);
+        rec_func(1, nums[1]);
         sb.append(max).append('\n').append(min);
         System.out.println(sb.toString());
     }
