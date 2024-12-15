0f178d4d7c55d49d9b1f0b1702778a2c62154bfe
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-15 18:37:32 +0900

    [Gold V] Title: 두 용액, Time: 900 ms, Memory: 112512 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,46 +18,28 @@ public class Main {
         }
     }
 
-    static int lower_bound(int[] A, int L, int R, int X) {
-        // A[L...R] 에서 X 이상의 수 중 제일 왼쪽 인덱스를 return하는 함수
-        // 그런 게 없다면 R + 1을 return 한다
-        int res = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] >= X) {
-                res = mid;
-                R = mid -1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        return res;
-    }
-
     static void pro() {
-        // A 에 대해 이분 탐색을 할 예정이니까, 정렬을 미리 해주자.
+        // A에 대해 이분 탐색을 할 예정이니까, 정렬을 미리 해주자
         Arrays.sort(A, 1, N + 1);
 
         int best_sum = Integer.MAX_VALUE;
-        int v1 = 0, v2 = 0;
-        for (int left = 1; left <= N; left++) {
-            // A[left] 용액을 쓸 것이다. 고로 -A[left] 와 가장 가까운 용액을 자신의 오른쪽 구간에서 찾자.
-            int res = lower_bound(A, left+1, N, -A[left]);
+        int v1 = 0, v2 = 0, L = 1, R = N;
 
-            // A[res - 1]와 A[res] 중에 A[left] 와 섞였을 때의 정보를 정답에 갱신시킨다.
-            if (left + 1 <= res - 1 && res - 1 <= N && Math.abs(A[res-1] + A[left]) < best_sum) {
-                best_sum = Math.abs(A[res-1] + A[left]);
-                v1 = A[left];
-                v2 = A[res - 1];
+        while(L < R) { // L==R 인 상황이면 용액이 한 개 뿐인 것이므로, L < R 일 때까지만 반복
+            /* TODO */
+            int sum = A[L] + A[R];
+            if (Math.abs(sum) < best_sum) {
+                best_sum = Math.abs(sum);
+                v1 = A[L];
+                v2 = A[R];
             }
 
-            if (left + 1 <= res && res <= N && Math.abs(A[res] + A[left]) < best_sum) {
-                best_sum = Math.abs(A[res] + A[left]);
-                v1 = A[left];
-                v2 = A[res];
+            if (sum > 0) {
+                R--;
+            } else {
+                L++;
             }
         }
-
         sb.append(v1).append(' ').append(v2);
         System.out.println(sb);
     }
