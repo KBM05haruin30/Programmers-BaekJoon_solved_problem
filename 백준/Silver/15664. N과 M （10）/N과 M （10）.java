f72662839d93838f91139f46ec9c16d2e9422db6
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-16 17:37:27 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,60 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] nums, selected, used;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N+1];
+        selected = new int[M+1];
+        used = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+    }
+
+    static void rec_func(int k, int start) {
+        if (k == M+1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            int lastUsed = 0;
+            for (int cand = start; cand <= N; cand++) {
+                if (used[cand] == 0 && nums[cand] != lastUsed) {
+                    selected[k] = nums[cand];
+                    used[cand] = 1;
+                    lastUsed = nums[cand];
+                    rec_func(k + 1, cand + 1);
+                    selected[k] = 0;
+                    used[cand] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        rec_func(1, 1);
+        System.out.println(sb.toString());
+    }
+
+}
