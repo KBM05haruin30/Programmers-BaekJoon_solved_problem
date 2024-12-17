a1aec42141fbc125fc79542971de51a9ce95329d
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-17 16:26:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 232 ms, Memory: 23244 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,91 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M, V;
+    static int[][] adj;
+    static boolean[] visit;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        adj = new int[N + 1][N + 1];
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            adj[x][y] = 1;
+            adj[y][x] = 1;
+        }
+    }
+
+    // x를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x) {
+        /* TODO */
+        visit[x] = true;
+        sb.append(x).append(' ');
+        for (int y = 1; y <= N; y++) {
+            if (adj[x][y] == 0) continue;
+            if (visit[y]) continue;
+            dfs(y);
+        }
+    }
+
+    // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
+    static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        /* TODO */
+
+        que.add(start);
+        visit[start] = true;
+
+        while(!que.isEmpty()) {
+            start = que.poll();
+            sb.append(start).append(' ');
+            for (int y = 1; y <= N; y++) {
+                if (adj[start][y] == 0) continue;
+                if (visit[y]) continue;
+
+                que.add(y);
+                visit[y] = true;
+            }
+        }
+
+    }
+
+    static void pro() {
+        // 모든 x에 대해서 adj[x] 정렬하기
+        /* TODO */
+        visit = new boolean[N + 1];
+
+        // DFS, BFS 결과 구하기
+        /* TODO */
+        dfs(V);
+        sb.append('\n');
+        for(int i = 1; i <= N; i++) {
+            visit[i] = false;
+        }
+        bfs(V);
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
+}
