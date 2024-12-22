b36017c8c01e0dd66d30d4b4679126b0c2632053
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-22 19:12:17 +0900

    [Silver III] Title: N과 M (7), Time: 556 ms, Memory: 123264 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,42 +1,50 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
+
     static StringBuilder sb = new StringBuilder();
 
     static int N, M;
     static int[] nums, selected;
 
-    static void input() {
-        N = sc.nextInt();
-        M = sc.nextInt();
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         nums = new int[N + 1];
-        selected = new int[N + 1];
+        selected = new int[M + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            nums[i] = sc.nextInt();
+            nums[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
     }
 
     static void rec_func(int k) {
         if (k == M + 1) {
-            for(int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(" ");
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
             }
             sb.append('\n');
         } else {
-            for(int cand = 1; cand <= N; cand++) {
+            for (int cand = 1; cand <= N; cand++) {
                 selected[k] = nums[cand];
                 rec_func(k + 1);
                 selected[k] = 0;
             }
         }
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         rec_func(1);
         System.out.println(sb.toString());
