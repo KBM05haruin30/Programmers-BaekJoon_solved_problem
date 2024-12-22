419e3d459383b57c88fbbdbdcf2068adde85f581
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-22 19:02:27 +0900

    [Silver III] Title: 파일 정리, Time: 732 ms, Memory: 64472 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,57 +1,52 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static StringBuilder sb = new StringBuilder();
-    static Scanner sc = new Scanner(System.in);
 
-    static int N;
+    static int N, cnt;
     static String[] file;
-    static String[] extension;
+    static String filename;
 
-    static void input() {
-        N = sc.nextInt();
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
         file = new String[N];
-        extension = new String[N];
         for (int i = 0; i < N; i++) {
-            file[i] = sc.next();
+            st = new StringTokenizer(br.readLine());
+            filename = st.nextToken();
+            file[i] = filename.split("\\.")[1];
         }
+        Arrays.sort(file);
     }
 
     static void pro() {
-        // .을 기준으로 확장자 명만 따로 저장하는 배열 만들기
-        for (int i = 0; i < N; i++) {
-            extension[i] = file[i].split("\\.",2)[1];
-        }
-        // 배열 정렬
-        Arrays.sort(extension);
-        // count: 등장 횟수
-        int count = 1;
-        String ext = extension[0];
-
-        // TODO
-        // 2번 원소부터 차례대로 보면서, 같은 확장자가 이어서 나오는지, 새로운 확장자가 나오는지
-        // 판단하여 count를 갱신해주고, 새로운 확장자의 이름과 값을 출력
+        String last = file[0];
+        cnt = 1;
         for (int i = 1; i < N; i++) {
-            if (extension[i].equals(extension[i-1])) {
-                count++;
+            if (last.equals(file[i])) {
+                cnt++;
             } else {
-                sb.append(ext).append(' ').append(count);
-                sb.append('\n');
-                ext = extension[i];
-                count = 1;
+                sb.append(last).append(' ').append(cnt).append('\n');
+                last = file[i];
+                cnt = 1;
             }
         }
-        sb.append(ext).append(' ').append(count);
-        System.out.println(sb.toString());
+        sb.append(last).append(' ').append(cnt).append('\n');
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        System.out.println(sb.toString());
     }
-
 
 }
