a88a7120b042598c26e24a8df149659e3b5e3956
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-22 19:05:55 +0900

    [Bronze III] Title: 세 수, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,30 +1,32 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
 
-    static int[] A;
+    static int[] arr;
 
-    static void input() {
-        A = new int[4];
-        for (int i = 1; i < 4; i++) {
-            A[i] = sc.nextInt();
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
     }
 
     static void pro() {
-        // TODO
-        // 입력 값 정렬
-        Arrays.sort(A);
-        // 두 번째로 큰 정수 출력
-        System.out.println(A[2]);
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
     }
+
 }
