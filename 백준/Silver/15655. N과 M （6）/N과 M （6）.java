dae17e511871caa7ccd88d0ca16aa80087a6417f
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2024-12-22 18:41:35 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,22 +1,30 @@
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static StringBuilder sb = new StringBuilder();
-    static Scanner sc = new Scanner(System.in);
 
     static int N, M;
-    static int[] nums, selected;
+    static int[] nums, selected, used;
 
-    static void input() {
-        N = sc.nextInt();
-        M = sc.nextInt();
-        nums = new int[N + 1];
-        selected = new int[M + 1];
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N+1];
+        used = new int[N+1];
+        selected = new int[M+1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            nums[i] = sc.nextInt();
+            nums[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
     }
@@ -28,18 +36,24 @@ public class Main {
             }
             sb.append('\n');
         } else {
-            for(int cand = start; cand <= N; cand++) {
+            for (int cand = start; cand <= N; cand++) {
+                if (used[cand] == 1) continue;
                 selected[k] = nums[cand];
+                used[cand] = 1;
                 rec_func(k + 1, cand + 1);
+                used[cand] = 0;
                 selected[k] = 0;
             }
+
         }
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         rec_func(1, 1);
         System.out.println(sb.toString());
     }
+
 
 }
