79e0929ded99483467687da9da02f7198155d4cd
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2025-10-31 13:51:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 324 ms, Memory: 40976 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+
+        while (st.hasMoreTokens()) {
+           st.nextToken();
+           count++;
+        }
+
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
