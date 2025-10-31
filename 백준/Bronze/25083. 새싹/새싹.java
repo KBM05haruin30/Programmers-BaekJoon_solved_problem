034c86c16057cceab4eee377834ad783da1ae212
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2025-10-31 16:12:26 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("         ,r\'\"7").append("\n")
+                  .append("r`-_   ,\'  ,/").append("\n")
+                  .append(" \\. \". L_r\'").append("\n")
+                  .append("   `~\\/").append("\n")
+                  .append("      |").append("\n")
+                  .append("      |");
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
