25ef27bc67d5a02bde9f8809456b922efd92580b
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2025-11-01 15:28:43 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int x =1,y = 1, max = -1;
+
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (max < num) {
+                    x = i+1;
+                    y = j+1;
+                    max = num;
+                }
+            }
+        }
+
+        sb.append(max).append("\n").append(x).append(" ").append(y);
+
+        bw.write(sb.toString());
+        bw.flush();
+
+
+    }
+}
