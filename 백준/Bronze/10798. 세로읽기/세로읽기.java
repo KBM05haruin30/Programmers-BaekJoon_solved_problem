7b827de0a3220e122b9e0ddc129170832527f377
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2025-11-02 14:22:35 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        String[][] words = new String[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < 15; j++) {
+                if (j < str.length()) {
+                    words[i][j] = String.valueOf(str.charAt(j));
+                } else {
+                    words[i][j] = "";
+                }
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                sb.append(words[j][i]);
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        
+    }
+}
